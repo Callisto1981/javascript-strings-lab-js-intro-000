a887288c92f3564d59097a0ace4237b913195cd2
fortunate-rockstar-9906 <wabbruscatoorr@gmail.com>
2019-02-21 16:28:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil DeGrasse Tyson!";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Lert's talk about space";
+var topic = "Let's talk about";
 
-var conversation = `High ${3+2}`;
+var conversation = `${topic}`;
